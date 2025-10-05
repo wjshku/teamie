5715c4a86237974c12ef6e1f8b2ef4a26f668dcc
junshi-wang <wangjunshi529@gmail.com>
2025-10-05 15:14:17 +0800

    Revert "Backend built with firebase"
    This reverts commit 835f0111cd79b0ef385fd3a13db803b80cb638d6.

--- backend/functions/lib/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// ==================== API Types Definition ====================
-// 基于数据接口文档的统一类型定义
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
